f3f12e7c4cdaccdd43156376d40698cff11450dc
ajeetraj11 <ajitpandey110920@gmail.com>
2025-02-23 19:30:37 +0530

    [Ajeet Raj] Add. Create a Helloworld Spring App to display message

--- src/main/java/com/springdemo/spring/SpringDemoApplication.java
+++ src/main/java/com/springdemo/spring/SpringDemoApplication.java
@@ -1,4 +1,4 @@
-package com.springdemo.Spring_Demo;
+package com.springdemo.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
